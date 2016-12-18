3470adbcd4b8042980ff8bec6d0d86947dd60dc9
Alex Tarng <alecadad@gmail.com>
2016-12-18 14:34:59 -0800

    Init project

---
+++ 2017 Scouting Collection/2017 Scouting Collection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2017 Scouting Collection
+//
+//  Created by Alex Tarng on 12/18/16.
+//  Copyright © 2016 Paly Robotics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
