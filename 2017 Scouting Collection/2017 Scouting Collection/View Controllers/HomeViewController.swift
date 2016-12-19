c51b1dc411e8eaa4c460ae888e4c50510aecc15f
Alex Tarng <alecadad@gmail.com>
2016-12-18 16:28:11 -0800

    Basic layout of home page and scout new match page

---
+++ 2017 Scouting Collection/2017 Scouting Collection/View Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  2017 Scouting Collection
+//
+//  Created by Alex Tarng on 12/18/16.
+//  Copyright © 2016 Paly Robotics. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: ViewController {
+    //    @IBOutlet weak var myButton : UIButton!
+    //    @IBOutlet weak var myTextField : UITextField!
+    //
+    //    @IBAction func myButtonAction(sender: id)
+    @IBAction func homeUnwind(unwindSegue: UIStoryboardSegue) {
+        
+    }
+}
